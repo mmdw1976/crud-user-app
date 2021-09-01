3a784acefd5cf5886dc1ebb0fb2548cc696a79a4
Mario <mmdw1976@gmail.com>
2021-09-01 14:31:41 +0200

    modified App.js and UserForm.js to setup addUser

--- src/App.js
+++ src/App.js
@@ -24,13 +24,20 @@ function App() {
 
   const [users, setUsers] = useState(userData);
 
+  const addUser = user => {
+    // because the id is counting on the previous state you must make use of prevState manner to avoid unexpected behaviour
+    setUsers((prevState) => {
+      return [...prevState, {...user, id: prevState.length + 1}];
+    })
+  }
+
   return (
     <div className="container">
       <h1>Crud with hooks</h1>
       <div className="flex-row">
         <div className="flex-large">
           <h2>Add User</h2>
-          <UserForm />
+          <UserForm addUser={addUser} />
         </div>
         <div className="flex-large">
           <h2>View Users</h2>
--- src/components/tables/UserForm.js
+++ src/components/tables/UserForm.js
@@ -1,23 +1,39 @@
 import React, {useState} from 'react'
 
-const UserForm = () => {
-    const [name, setName] = useState('');
-    const [username, setUsername] = useState('')
+const UserForm = (props) => {
+    const initialFormState = {id: null, name: '', username: ''};
 
-    const submitHandler = () => {
-        console.log({name, username})
+    const [user, setUser] = useState(initialFormState);
+
+    const handleInputChange = (e) => {
+        const {name, value} = e.target;
+
+        setUser(
+            {...user, [name]: value}
+        )
+    }
+
+    const submitHandler = (e) => {
+        console.log('from form');
+        console.log(user)
+        e.preventDefault();
+        props.addUser(user);
     }
     return ( 
         <form onSubmit={submitHandler}>
             <div>
                 <label htmlFor="name">Name</label>
                 <br />
-                <input type="text" name="name" id="name" />
+                <input type="text" name="name" id="name" value={user.name} onChange={handleInputChange} />
             </div>
             <div>
                 <label htmlFor="username">Username</label>
                 <br />
-                <input type="text" name="username" id="username" />
+                <input type="text" name="username" id="username" value={user.username} onChange={handleInputChange} />
+            </div>
+            <br />
+            <div>
+                <button>submit</button>
             </div>
         </form>
      );
